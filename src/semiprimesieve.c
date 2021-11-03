c40a9f065a1b223e0b49dd221eecbd74fb916b59
Will Brown <kyrosewoodgang@gmail.com>
2021-11-02 20:41:52 -0400

    Don't pre-declare variables

--- src/semiprimesieve.c
+++ src/semiprimesieve.c
@@ -18,14 +18,11 @@ void findSemiprimes(long limit) {
 	/* We are going to use a bit array to save on memory and make our code faster,
 	 * so we will allocate an array based on the limit and fill it with ones. */
 	semiprimes = makeBitArray(limit + 1, 0);
-	 
-	long i = 0;
-	long j = 0;
-	long multiple = 0;
+	
 	// Loop through primeList, multiplying each number with the rest of the list.
-	for (i = 0L; i < primeList.length; i++)
-		for (j = i; j < primeList.length; j++) {
-			multiple = primeList.list[i] * primeList.list[j];
+	for (long i = 0L; i < primeList.length; i++)
+		for (long j = i; j < primeList.length; j++) {
+			long multiple = primeList.list[i] * primeList.list[j];
 			if (multiple <= limit) {
 				SetBit(semiprimes, multiple);
 				numSemiprimes++;
