b5fd555fa98c72c49a1b93052b0d5ebca637e39d
Will Brown <kyrosewoodgang@gmail.com>
2021-11-15 23:44:17 -0500

    [primesieve.c] Move count method into bitcount.h

--- src/bitcount.h
+++ src/bitcount.h
@@ -22,4 +22,22 @@ static const char BITCOUNT[256] = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3,
 									6, 7, 4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7,
 									6, 7, 7, 8 };
 
+static long countBits(int *bitArray, long limit) {
+	// Cast bitarray to an unsigned char array so we can count each byte
+	unsigned char *arrayBytes = (unsigned char*) bitArray;
+	
+	// Get # of contiguous bytes (quantity we can count fast without bitops)
+	long fastCountLen = (limit + 1) / 8;
+	
+	// Count each byte by adding its value from the BITCOUNT LUT to numOn
+	long numOn = 0L;
+	for (long i = 0L; i < fastCountLen; i++)
+		numOn += BITCOUNT[arrayBytes[i]];
+	
+	// Count the remaining bits bit by bit
+	for (long i = fastCountLen * 8 + 1; i <= limit; i++)
+		if (arrayBytes[i/8] & (BITMASKS[i%8]))
+			numOn++;
+}
+
 #endif
--- src/primesieve.c
+++ src/primesieve.c
@@ -11,24 +11,6 @@
 long numPrimes;
 int32_t *primes;
 
-void countPrimes(long limit) {
-	// Cast primes to an unsigned char array so we can count each byte
-	unsigned char *primeBytes = (unsigned char*) primes;
-	
-	// Get # of contiguous bytes (ones we can count fast without bitops)
-	long fastCountLen = (limit + 1) / 8;
-	
-	// Count each byte by adding its value from the BITCOUNT LUT to numPrimes
-	numPrimes = 0L;
-	for (long i = 0L; i < fastCountLen; i++)
-		numPrimes += BITCOUNT[primeBytes[i]];
-	
-	// Count the remaining bits bit by bit
-	for (long i = fastCountLen * 8 + 1; i <= limit; i++)
-		if (primeBytes[i/8] & (BITMASKS[i%8]))
-			numPrimes++;
-}
-
 long findPrimes(long limit) {
 	/* We are going to use a bit array to save on memory and make our code faster,
 	 * so we will allocate an array based on the limit and fill every byte with
@@ -46,7 +28,7 @@ long findPrimes(long limit) {
 			for (long multiple = num * num; multiple <= limit; multiple += 2 * num)
 				ClearBit(primes, multiple);
 	
-	countPrimes(limit);
+	countBits(primes, limit);
 	return numPrimes;
 }
 
