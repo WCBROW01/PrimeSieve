1fcad8ee39fd4043c6e1d56f2f115801fec7dcee
Will Brown <kyrosewoodgang@gmail.com>
2021-11-15 22:13:20 -0500

    Count primes afterwards byte by byte
    Instead of counting all composite numbers while finding primes, count the
    number of primes afterwards byte by byte. This removes a branch and uses a
    LUT for the number of active bits in each byte value. Since each byte is
    touched only once and no bitmasks are requred, this is a much faster method of
    counting.

--- src/primesieve.c
+++ src/primesieve.c
@@ -5,14 +5,31 @@
 #include <math.h>
 
 #include "bitops.h"
+#include "bitcount.h"
 #include "primesieve.h"
 
 long numPrimes;
 int32_t *primes;
 
-long findPrimes(long limit) {
-	long numComposite = limit / 2; // All multiples of 2 are already marked.
+void countPrimes(long limit) {
+	// Cast primes to an unsigned char array so we can count each byte
+	unsigned char *primeBytes = (unsigned char*) primes;
 	
+	// Get # of contiguous bytes (ones we can count fast without bitops)
+	long fastCountLen = (limit + 1) / 8;
+	
+	// Count each byte by adding its value from the BITCOUNT LUT to numPrimes
+	numPrimes = 0L;
+	for (long i = 0L; i < fastCountLen; i++)
+		numPrimes += BITCOUNT[primeBytes[i]];
+	
+	// Count the remaining bits bit by bit
+	for (long i = fastCountLen * 8 + 1; i <= limit; i++)
+		if (primeBytes[i/8] & (BITMASKS[i%8]))
+			numPrimes++;
+}
+
+long findPrimes(long limit) {
 	/* We are going to use a bit array to save on memory and make our code faster,
 	 * so we will allocate an array based on the limit and fill every byte with
 	 * a value that pre-mark every even number. */
@@ -27,12 +44,9 @@ long findPrimes(long limit) {
 		if (CheckBit(primes, num))
 			// Mark all odd multiples after square of prime
 			for (long multiple = num * num; multiple <= limit; multiple += 2 * num)
-				if (CheckBit(primes, multiple)) {
-					ClearBit(primes, multiple);
-					numComposite++;
-				}
+				ClearBit(primes, multiple);
 	
-	numPrimes = limit - numComposite;
+	countPrimes(limit);
 	return numPrimes;
 }
 
