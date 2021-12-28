5a14d5e778a7af33cfe657a9f676214eb54d9e75
cpsdqs <cpsdqs@gmail.com>
2021-12-28 17:16:47 +0100

    Fix all concurrency bugs forever

--- src/primesieve.c
+++ src/primesieve.c
@@ -10,7 +10,8 @@
 #include "primesieve.h"
 
 
-#define max(a, b) ((a) > (b)) ? (a) : (b)
+#define min(a, b) ((a) > (b)) ? (b) : (a)
+#define next_multiple(a, b) (a) + ((b) - (a) % (b))
 
 long numPrimes;
 BitArray *primes;
@@ -29,24 +30,33 @@ static inline unsigned long lsqrt(long n) {
 
 typedef struct MarkerThread {
 	pthread_t thread;
-	long num_end;
-	long start_offset;
-	long end_offset;
 	uint32_t* primes;
-	atomic_long* main_cursor;
+	unsigned long check_end;
+	atomic_ulong* check_barrier;
+	/** Range start number, exclusive. */
+	unsigned long range_start;
+	/** Range end number, inclusive. */
+	unsigned long range_end;
 } MarkerThread;
 
 static void* marker_thread_fn(void* arg) {
 	MarkerThread tdata = *((MarkerThread *)arg);
-	printf("thread range %ld - %ld\n", tdata.start_offset, tdata.end_offset);
+
 	long num = 3L;
-	while (num < tdata.num_end) {
-		long main_cursor = atomic_load_explicit(tdata.main_cursor, memory_order_acquire);
-		while (num < main_cursor) {
+	while (num <= tdata.check_end) {
+		// Spinning here is fine, since it will very likely only be for a short while.
+		long main_cursor = atomic_load_explicit(tdata.check_barrier, memory_order_acquire);
+
+		while (num <= main_cursor) {
 			if (CheckBit(tdata.primes, num / 2)) {
-				long multiple = num * num;
-				while (multiple < tdata.start_offset) multiple += 2 * num;
-				for (; multiple <= tdata.end_offset; multiple += 2 * num) {
+				unsigned long multiple = num * num;
+
+				// Seek to this thread's assigned range
+				// FIXME: this can probably be an analytic expression
+				while (multiple <= tdata.range_start) multiple += 2 * num;
+
+				// Mark all multiples in this thread's assigned range
+				for (; multiple <= tdata.range_end; multiple += 2 * num) {
 					ClearBit(tdata.primes, multiple / 2);
 				}
 			}
@@ -56,31 +66,52 @@ static void* marker_thread_fn(void* arg) {
 	return NULL;
 }
 
+/*
+ * The range represented by a single item of the BitArray.
+ * uint32_t represents 32 numbers, and since even numbers don't exist, this is doubled to 64.
+ * Thread boundaries will be placed at multiples of this value such that each thread only touches
+ * its own BitArray items.
+ */
+#define ATOM_SIZE 64
+
 long findPrimes(long limit) {
 	/* We are going to use a bit array to save on memory and make our code faster,
 	 * so we will allocate an array based on the limit, excluding even numbers. */
 	primes = makeBitArray(limit / 2, 0xFF);
 
-	int thread_count = sysconf(_SC_NPROCESSORS_ONLN) - 1;
-	printf("use %d threads\n", thread_count);
+	long thread_count = sysconf(_SC_NPROCESSORS_ONLN) - 1;
+
+	// The main thread's assigned range ranges from 0 to main_range_end.
+	// This is the last item (inclusive) that will be potentially written to by the main thread.
+	unsigned long main_range_end = next_multiple(lsqrt(limit), ATOM_SIZE);
+
+	// All remaining numbers will be marked by marker threads.
+	unsigned long remaining_range = next_multiple(limit - main_range_end, ATOM_SIZE);
+
+	// Only create as many threads as we need.
+	thread_count = min(thread_count, remaining_range / ATOM_SIZE);
+
+	printf("using %ld thread(s)\n", thread_count);
 	MarkerThread* threads = calloc(thread_count, sizeof(MarkerThread));
 
-	atomic_long main_cursor = 0;
-	pthread_attr_t attr;
-	pthread_attr_init(&attr);
+	// The largest number of which all multiples have been marked in the main thread's assigned
+	// range. This will be used as a memory barrier for threads.
+	atomic_ulong check_barrier = 2;
+
+	// Create the threads!
 	for (int i = 0; i < thread_count; i++) {
 		threads[i].primes = primes;
-		threads[i].main_cursor = &main_cursor;
-		threads[i].start_offset = max(lsqrt(limit), (limit / 32) * i / thread_count * 32);
-		threads[i].end_offset = max(lsqrt(limit), (limit / 32) * (i + 1) / thread_count * 32);
-		threads[i].num_end = lsqrt(limit);
-		int res = pthread_create(&threads[i].thread, &attr, &marker_thread_fn, &threads[i]);
+		threads[i].check_barrier = &check_barrier;
+		threads[i].check_end = lsqrt(limit);
+		threads[i].range_start = main_range_end + remaining_range / ATOM_SIZE * i / thread_count * ATOM_SIZE;
+		threads[i].range_end = min(limit, main_range_end + remaining_range / ATOM_SIZE * (i + 1) / thread_count * ATOM_SIZE);
+
+		int res = pthread_create(&threads[i].thread, NULL, &marker_thread_fn, &threads[i]);
 		if (res != 0) {
 			printf("FATAL: failed to create thread: %d\n", res);
 			exit(-1);
 		}
 	}
-	pthread_attr_destroy(&attr);
 
 	// Predefine values that we're skipping in the sieve.
 	ClearBit(primes, 0);
@@ -89,17 +120,17 @@ long findPrimes(long limit) {
 	for (long num = 3L; num <= lsqrt(limit); num += 2) {
 		if (CheckBit(primes, num / 2)) {
 			// Mark all odd multiples after square of prime
-			for (long multiple = num * num; multiple <= lsqrt(limit); multiple += 2 * num) {
+			for (long multiple = num * num; multiple <= main_range_end; multiple += 2 * num) {
 				ClearBit(primes, multiple / 2);
 			}
 
-			atomic_store_explicit(&main_cursor, num, memory_order_release);
+			atomic_store_explicit(&check_barrier, num, memory_order_release);
 		}
 	}
 
-	printf("waiting for threads to join...\n");
-	atomic_store_explicit(&main_cursor, lsqrt(limit), memory_order_release);
-	for (int i = 0; i < thread_count; i++) {
+	// Signal that the main thread is done, and wait for threads to join.
+	atomic_store_explicit(&check_barrier, lsqrt(limit), memory_order_release);
+	for (long i = 0; i < thread_count; i++) {
 		pthread_join(threads[i].thread, NULL);
 	}
 	free(threads);
