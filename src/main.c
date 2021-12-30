8d7e3990106aa87d7cbcdfb08e4f5d02a67e540f
Will Brown <kyrosewoodgang@gmail.com>
2021-12-29 19:42:32 -0500

    [main.c] Fix printPrimes to not check out of bounds

--- src/main.c
+++ src/main.c
@@ -9,8 +9,8 @@
 static void printPrimes(void) {
 	BitList primeList = listBits(numPrimes, primes);
 
-	puts("Primes found: 2");
-	for (long i = 0L; i < primeList.length; i++)
+	printf("Primes found: 2");
+	for (long i = 0L; i < primeList.length - 1; i++)
 		printf(", %ld", 2 * primeList.list[i] + 1);
 
 	printf("\n");
