8b3e632f6aa320a2bcbb42efec7c5fbbac660056
Will Brown <kyrosewoodgang@gmail.com>
2022-01-02 22:25:24 -0500

    [primesievejni.c] Edit behavior to reflect numPrimes change

--- src/primesievejni.c
+++ src/primesievejni.c
@@ -1,29 +1,31 @@
 #include "primesievejni.h"
 #include "primesieve.h"
 
+static long numPrimes;
+
 JNIEXPORT void JNICALL Java_PrimeSieve_findPrimes
   (JNIEnv *, jobject, jlong limit) {
-	findPrimes(limit);
+	numPrimes = findPrimes(limit);
 }
 
 JNIEXPORT jlongArray JNICALL Java_PrimeSieve_listPrimes
-  (JNIEnv *env, jobject) { 
+  (JNIEnv *env, jobject) {
 	if (primes == NULL) {
 		printf("Primes have not been generated yet.\n");
 		return NULL;
 	}
-	
+
 	BitList primeList = listBits(numPrimes, primes);
 	free(primes);
-	
+
 	jlongArray newPrimeList = (*env)->NewLongArray(env, primeList.length);
-	
+
 	if (newPrimeList == NULL)
 		return NULL; // out of memory error thrown
-	
+
 	(*env)->SetLongArrayRegion(env, newPrimeList, 0, primeList.length, primeList.list);
 	free(primeList.list);
-	return newPrimeList; 
+	return newPrimeList;
 }
 
 JNIEXPORT jint JNICALL Java_PrimeSieve_getNumPrimes
